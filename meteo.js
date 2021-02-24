3277996cc8713207f82eadc044cce4eb1b81ba54
SyMame <sy.mame.diack@gmail.com>
2021-02-24 16:12:42 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -4,7 +4,7 @@ var callBackGetSuccess = function(data){
 }
 
 function buttonClickGET(){
-    var url=""
+    var url="https://data.opendatasoft.com/api/records/1.0/search/?dataset=observation-meteorologique-historiques-france-synop%40grandparissud&q=&sort=date&facet=date&facet=nom&facet=temps_present&facet=libgeo&facet=nom_epci&facet=nom_dept&facet=nom_reg&timezone=Europe%2FParis"
 
     $.get(url, callBackGetSuccess).done(function(){
     })
