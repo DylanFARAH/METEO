25b039d7f7862b3c1422c274352dc0cf96fb2d4e
SY Mame <sy.mame.diack@gmail.com>
2021-02-25 02:41:08 +0100

    style1

--- meteo.js
+++ meteo.js
@@ -2,10 +2,13 @@ var callBackGetSuccess = function(data){
     console.log("donnees meteo",data)
     //alert("la temperature est de:" + data.records[0].fields.tc)
     var element = document.getElementById("zone_meteo");
-    element.innerHTML = "La temeprature est de " +data.records[0].fields.tc;
+    element.innerHTML = "La temperature est de " +data.records[0].fields.tc;
+    var element = document.getElementById("vent");
+    element.innerHTML = "La vitesse du vent est de " +data.records[0].fields.vv;
 }
 
 function buttonClickGET(){
+
     var url="https://data.opendatasoft.com/api/records/1.0/search/?dataset=observation-meteorologique-historiques-france-synop%40grandparissud&q=&sort=date&facet=date&facet=nom&facet=temps_present&facet=libgeo&facet=nom_epci&facet=nom_dept&facet=nom_reg&timezone=Europe%2FParis"
 
     $.get(url, callBackGetSuccess).done(function(){
