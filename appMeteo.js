4f2e8056148bc4b044502c01601b29f160f3684a
Dylan FARAH <48685231+DylanFARAH@users.noreply.github.com>
2021-03-11 23:11:08 +0100

    Update appMeteo.js

--- appMeteo.js
+++ appMeteo.js
@@ -1,97 +1,45 @@
-var callBackGetSuccess = function(data){
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="appMeteo.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/css/select2.min.css">
 
-    var donnee=data;
+        <title>meteo</title>
+    </head>
+    <body style="display: inline;">
+        <div>
+                <div id="card" class="container mt-4" >
+                    <h1>Météo</h1>                
+                    <input id="longitude" type="number" placeholder="longitude" class="input_text" required="required">
+                    <input id="latitude" type="number" placeholder="latitude" class="input_text" required="required">
+                    <input id="rayon" type="number" placeholder="rayon (en Km)" class="input_text" required="required">
+                    <input type="submit" value="Rechercher" class="btn btn-primary" onclick="buttonClickGET()"/>  
+                    <input type="submit" class="btn btn-secondary" value="CSV" class="submit" onclick="createCSV()"/> 
 
-    if(document.getElementsByTagName("tbody")[0]){
-        let d = document.getElementsByTagName("table")[0];
-        let d_nested = document.getElementsByTagName("tbody")[0];
-        d.removeChild(d_nested);
-      }
-
-        // get the reference for the table
-        var tbl = document.getElementsByTagName("table")[0];
-  
-        // creates a <table> element and a <tbody> element
-        var tblBody = document.createElement("tbody");
-      
-        // creating all cells
-          
-            // Create a <td> element and a text node, make the text
-            // node the contents of the <td>, and put the <td> at
-            // the end of the table row
-            donnee.records.forEach(aux => {
-            var row = document.createElement("tr");
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.date);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.nom);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.nom_reg);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.nom_dept);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.t);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.dd);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.ff);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.u);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.etat_sol);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-    
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(aux.fields.ht_neige);
-            cell.appendChild(cellText);
-            row.appendChild(cell);          
-            
-            // add the row to the end of the table body
-            tblBody.appendChild(row);
-          });
-          // put the <tbody> in the <table>
-          tbl.appendChild(tblBody);
-
-}
-
-function buttonClickGET(){
-    var longitude = document.getElementById("longitude").value;
-    var latitude = document.getElementById("latitude").value;
-    var rayon = document.getElementById("rayon").value * 1000;
-
-    var url="https://data.opendatasoft.com/api/records/1.0/search/?dataset=donnees-synop-essentielles-omm%40public&rows=100&sort=date&timezone=Europe%2FParis&geofilter.distance="+latitude+"%2C"+longitude+"%2C"+rayon
-
-    $.get(url, callBackGetSuccess).done(function(){
-    })
-    .fail(function(){
-        alert("error");
-    })
-    .always(function(){
-    })
-}
-
+                    <table class="table table-striped" style="margin-top:20px;">
+                    <thead>
+                        <tr>
+                            <th>date</th>
+                            <th>nom</th>
+                            <th>région</th>
+                            <th>département</th>
+                            <th>température</th>
+                            <th>direction du vent</th>
+                            <th>vitesse du vent</th>
+                            <th>humidité</th>
+                            <th>état du sol</th>
+                            <th>hauteur d'un neige</th>
+                        </tr>
+                    </thead>
+                </table>
+                </div>
+        </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/js/select2.min.js"></script>
+    </body>
+</html>
